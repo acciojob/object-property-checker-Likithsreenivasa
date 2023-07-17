7a2cbf1194a03789d28afd03e2d5bb452e432c16
Likithsreenivasa <118590713+Likithsreenivasa@users.noreply.github.com>
2023-07-17 09:02:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	return key in sampleObject;
+	if(key in sampleObject)
+	{
+		return true;
+	}
+	return false;
 	
 }
-
 // Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
